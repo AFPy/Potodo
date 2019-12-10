7a7b2cfb4882a3c29fda51dabbe52602c4dba5ab
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2019-12-10 21:04:18 +0100

    Added new PoFile vars

--- potodo/_po_file.py
+++ potodo/_po_file.py
@@ -20,14 +20,28 @@ class PoFile:
         self.untranslated_entries: list = self.pofile.untranslated_entries()
         self.untranslated_nb: int = len(self.untranslated_entries)
 
+        self.obsolete_entries: list = self.pofile.obsolete_entries()
+        self.obsolete_nb: int = len(self.pofile.obsolete_entries())
+
+        self.po_file_size = len(self.pofile) - self.obsolete_nb
+
+    def get_dir_and_filename(self, directory):
+        return directory + "/" + self.filename
+
     def __str__(self):
-        return f"Filename: {self.filename}\nFuzzy Entries: {self.fuzzy_entries}\nPercent Translated: {self.percent_translated}\nTranslated Entries: {self.translated_entries}\nUntranslated Entries: {self.untranslated_entries}"
+        return f"Filename: {self.filename}\n" \
+               f"Fuzzy Entries: {self.fuzzy_entries}\n" \
+               f"Percent Translated: {self.percent_translated}\n" \
+               f"Translated Entries: {self.translated_entries}\n" \
+               f"Untranslated Entries: {self.untranslated_entries}"
 
+    def __lt__(self, other):
+        return self.filename < other.filename
 
-def get_po_files(path: str):
-    po_files = [file for file in Path(path).glob("**/*.po") if ".git/" not in str(file)]
+def get_po_files_from_repo(repo_path: str):
+    all_po_files = [file for file in Path(repo_path).glob("**/*.po") if ".git/" not in str(file)]
     po_files_per_directory = {
-        path.parent.name: set(path.parent.glob("*.po")) for path in po_files
+        path.parent.name: set(path.parent.glob("*.po")) for path in all_po_files
     }
     end_dict = {}
     for directory, po_files in sorted(po_files_per_directory.items()):
