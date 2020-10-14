733e0ffbc1f0e749085603c0d875fb9ba7d8664e
Jules Lasne (jlasne - seluj78) <jules.lasne@gmail.com>
2020-10-14 09:46:50 +0200

    Added better ignore patter for potodoignore + Added caching of passed args for better cache handling

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     package_dir={"potodo": "potodo"},
     entry_points={"console_scripts": ["potodo=potodo.potodo:main"]},
     include_package_data=True,
-    install_requires=["polib", "requests", "simple-term-menu"],
+    install_requires=["polib", "requests", "simple-term-menu", "gitignore_parser"],
     license="MIT license",
     zip_safe=False,
     keywords="potodo",
